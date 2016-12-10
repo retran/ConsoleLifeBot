1ca01736f5082e9cfd6754fe0015588053ffaa3e
Andrew Vasilyev <a.vasilyev@corvusalba.ru>
2016-12-10 23:07:31 +0300

    initial commit

---
+++ src/main/java/me/retran/consolelifebot/App.java
@@ -0,0 +1,10 @@
+package me.retran.consolelifebot;
+
+/**
+ * Created by retran on 12/10/16.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
