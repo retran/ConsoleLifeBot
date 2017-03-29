18b6a5692782f3e878fd203e44c66141a5aee3a1
Andrew Vasilyev <a.vasilyev@corvusalba.ru>
2017-03-29 19:46:39 +0300

    remove scala project

--- build.sbt
+++
@@ -1,6 +0,0 @@
-name := "ConsoleLifeBot"
-
-version := "1.0"
-
-scalaVersion := "2.12.1"
-
--- src/main/scala/Application.scala
+++
@@ -1,7 +0,0 @@
-package me.retran.consolelifebot {
-  object Application {
-    def main(args: Array[String]): Unit = {
-      println("Hello World!");
-    }
-  }
-}
